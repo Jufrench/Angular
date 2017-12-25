43316478717a8735a21d071ffb13051a71477513
Julian French <ju.french@gmail.com>
2017-12-24 22:01:40 -0600

    Single Page Application Part 1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { ContactComponent } from './contact/contact.component';
 // ==== Services
 // =============
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 // ==== Modules
 // =============
@@ -43,7 +44,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
